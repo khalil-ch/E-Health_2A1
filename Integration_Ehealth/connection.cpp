1b631c3754031861ff561feb4022331367dbf72f
khalil chettaoui <khalilassef.chettaoui@gmail.com>
2021-01-05 20:03:26 +0100

    commit aziz khalil

--- Integration_Ehealth/connection.cpp
+++ Integration_Ehealth/connection.cpp
@@ -8,9 +8,9 @@ connection::connection()
 bool connection::createconnect()
 {bool test=false;
 QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-db.setDatabaseName("Projet2A");//inserer le nom de la source de données ODBC
-db.setUserName("aziz");//inserer nom de l'utilisateur
-db.setPassword("aziz123");//inserer mot de passe de cet utilisateur
+db.setDatabaseName("test4");//inserer le nom de la source de données ODBC
+db.setUserName("khalil3");//inserer nom de l'utilisateur
+db.setPassword("khalil3");//inserer mot de passe de cet utilisateur
 
 if (db.open())
 test=true;
